eda5382ba63c067d262b3876c0aa99d55c8e3c17
Liza <gogald@yandex.ru>
2014-07-22 16:09:31 +0400

    unit cdata.h

--- checkers/src/client/lib/cdata.h
+++ checkers/src/client/lib/cdata.h
@@ -1,4 +1,5 @@
-//#include <>
+#include<unistd.h>
+#include"../../inc/global.h"
 
 int send_cdata(int, struct cdata);
 int recv_cdata(int, struct sdata*);
--- checkers/src/client/lib/recv_cdata.c
+++ checkers/src/client/lib/recv_cdata.c
@@ -1,3 +1,13 @@
-int recv_cdata(int, struct sdata*){
-  
+#include"cdata.h"
+/*
+ * receive message from server
+ * \param soc - socket
+ * \param data - received data
+ * \return -1 - error
+ */
+int recv_cdata(int soc,struct sdata* data){
+  if(read(soc,(void *)data,sizeof(struct sdata),0)<0){
+    return -1;
+  }
+  return 0;
 }
