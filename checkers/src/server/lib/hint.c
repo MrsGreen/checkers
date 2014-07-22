00edbe3fc722950a7cf98b6633ff7355220a01b2
Liza <gogald@yandex.ru>
2014-07-22 13:59:02 +0400

    add unit check_msg

--- checkers/src/server/lib/hint.c
+++ checkers/src/server/lib/hint.c
@@ -1,3 +1,19 @@
-int hint(int){
-  
+#include"check_msg.h"
+/*
+ * Send hint message
+ * \param soc - socket
+ * \return -1 - error
+ */
+int hint(int soc){
+  struct sdata data;
+  char text[]="Wrong message format. Please type help\n"
+
+  memset(data,0,sizeof(data));
+  strcpy(data.c_msg.msg,text);
+  data.c_msg.msg.soc=soc;
+
+  if(send_sdata(data)<0){
+    return -1;
+  }
+  return 0;
 }
