888fdf3795e3d3eabd466c9b1a055a81e66b87a2
Liza <gogald@yandex.ru>
2014-07-22 16:19:55 +0400

    fix send_cdata

--- checkers/src/client/lib/cdata.h
+++ checkers/src/client/lib/cdata.h
@@ -1,5 +1,5 @@
 #include<unistd.h>
 #include"../../inc/global.h"
 
-int send_cdata(int, struct cdata);
+int send_cdata(struct cdata);
 int recv_cdata(int, struct sdata*);
--- checkers/src/client/lib/send_cdata.c
+++ checkers/src/client/lib/send_cdata.c
@@ -1,12 +1,11 @@
 #include"cdata.h"
 /*
  * send message to server
- * \param soc - socket
  * \param data - client's data
  * \return -1 - error
  */
-int send_cdata(int soc, struct cdata data){
-  if(write(soc,(void *)data,sizeof(data),0)<0){
+int send_cdata(struct cdata data){
+  if(write(data.c_msg.soc,(void *)data,sizeof(data),0)<0){
     return -1;
   }
   return 0;
