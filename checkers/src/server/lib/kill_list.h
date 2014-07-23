db9859c1dc013500a9568228fc9f4c331c3659dc
Liza <gogald@yandex.ru>
2014-07-23 09:09:50 +0400

    add list unit

---
+++ checkers/src/server/lib/kill_list.h
@@ -0,0 +1,8 @@
+#include"../../inc/global.h"
+#include<malloc.h>
+
+void list_push(struct move_list **,struct move_data);
+void list_pop(struct move_list **,struct move_data);
+void list_add(struct move_list **,struct move_data);
+void list_clear(struct move_list **);
+int list_data(struct move_list **,struct move_data);
---
+++ checkers/src/server/lib/list_push.c
@@ -0,0 +1,26 @@
+#include"kill_list.h"
+/*
+ * push value val into list head
+ * \param head - head of list
+ * \param val - value
+ */
+void list_push(struct move_list ** head,struct move_data val){
+  struct move_list * p;
+  p=( struct move_list *)malloc(sizeof(struct move_list));
+  if(p==NULL){
+    printf("Push: malloc error\n");
+    return;
+  }
+
+  p->move.i1=val.i1;
+  p->move.j1=val.j1;
+  p->move.i2=val.i2;
+  p->move.j2=val.j2;
+
+  if(head==NULL){
+    p->next=NULL;
+  } else {
+    p->next=*head;
+  }
+  *head=p;
+}
